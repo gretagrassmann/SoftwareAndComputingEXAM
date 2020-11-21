9086e8594c7dde50d567bb1acfbf87ea532af6ae
Greta Grassmann <gretagrassmann0@gmail.com>
2020-11-21 19:33:51 +0100

    Depending on the name of the file with the testing data of the model that has to be compared with the original one, the correct label in the graphs are chosen.

--- avg_loss_plot_test.py
+++ avg_loss_plot_test.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-#from defs import *
 
-
-relu = "C:\\Users\\Cobal\\Desktop\\Testing_loss_noedge.txt"
-tanh = "C:\\Users\\Cobal\\Desktop\\Sigmoidal_Testing_loss_noedge.txt"
+original = "C:\\Users\\Cobal\\Desktop\\Testing_loss_noedge.txt"
+modified = "C:\\Users\\Cobal\\Desktop\\Sigmoidal_Testing_loss_noedge.txt"
 
 
 # Import data as a list of numbers
-with open(relu) as textFile:
+with open(original) as textFile:
     loss,model,roc_auc = np.loadtxt(fname=textFile,delimiter=',',
                                     skiprows=1,
                                     unpack=True)
@@ -17,7 +15,7 @@ with open(relu) as textFile:
     model = model[model_sort]
     roc_auc = roc_auc[model_sort]
 
-with open(tanh) as textFile:
+with open(modified) as textFile:
     loss1,model1,roc_auc1 = np.loadtxt(fname=textFile, delimiter=',',
                                         skiprows=1,
                                         unpack=True)
@@ -30,16 +28,26 @@ with open(tanh) as textFile:
 fig, (ax1,ax2) = plt.subplots(nrows=2, ncols=1)
 
 #PLOT THINGS ON CORRECT PLOTS
-ax1.plot(model, loss, '-o', label='Models with a ReLU activation function')
-ax1.plot(model1, loss1, '-o', label='Models with a tanh activation function')
-ax2.plot(model, roc_auc, '-o', label='Models with a ReLU activation function')
-ax2.plot(model1, roc_auc1, '-o', label='Models with a tanh activation function')
+if "Sigmoidal" in modified:
+    original_label = 'Models with ReLU as transition function'
+    modified_label = 'Models with tanh as transition function'
+elif "IndPar" in modified:
+    original_label = 'With the same weights for the two "legs"'
+    modified_label = 'With independent weights for the two "legs"'
+else:
+    original_label = 'Without the edges\' features'
+    modified_label = 'With the edges\' features'
+
+
+ax1.plot(model, loss, '-o', label= original_label)
+ax1.plot(model1, loss1, '-o', label= modified_label)
+ax2.plot(model, roc_auc, '-o', label= original_label)
+ax2.plot(model1, roc_auc1, '-o', label= modified_label)
 
 #SETTING OF ALL LABELS
 ax2.set_xlabel('Model trained with epoch number')
 ax2.set_ylabel('roc_auc')
 ax1.set_ylabel('Average loss for testing')
-fig.suptitle('Behaviour of loss and roc_auc with training process')
 ax1.legend()
 ax2.legend()
 plt.show()
